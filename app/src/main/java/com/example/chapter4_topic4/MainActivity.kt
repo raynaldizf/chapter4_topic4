41bed15f9a077e20ecb9f5ec7ccb4a1f1e20252e
surezluvy <raynaldizoel@gmail.com>
2022-09-19 19:44:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chapter4_topic4/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.chapter4_topic4
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.chapter4_topic4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var sharedPrefs : SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPrefs = getSharedPreferences("STUDENT",Context.MODE_PRIVATE)
+        binding.btnSave.setOnClickListener{
+            saveData()
+            Toast.makeText(this,"Data Berhasil Disimpan",Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun saveData(){
+        var getNim = binding.etNim.text.toString()
+        var getName = binding.etName.text.toString()
+
+        var addData = sharedPrefs.edit()
+        addData.putString("nim",getNim)
+        addData.putString("name",getName)
+        addData.apply()
+
+    }
+
+    fun showData(){
+        
+    }
+}
